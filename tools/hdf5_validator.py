8b67164858dfe436c980e9bf814ec961dde91670
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2025-10-23 09:23:39 -0400

    Update hdf5_validator.py

--- tools/hdf5_validator.py
+++ tools/hdf5_validator.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
-print('Validate HDF5')
+"""Minimal HDF5 validator for required groups/attributes."""
+import h5py, sys
+
+f=h5py.File(sys.argv[1],'r')
+reqs=['metadata','initial','evolution','extraction','waveforms','diagnostics']
+for r in reqs:
+    if r not in f:
+        print('MISSING',r)
+        sys.exit(2)
+print('Basic HDF5 layout OK')
