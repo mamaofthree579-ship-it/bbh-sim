360632519b152cfb9868ae2bf5a416f10f26217c
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2025-11-11 22:55:08 -0500

    Update and rename three.js to Three.js

--- multiverse/Three.js
+++ multiverse/Three.js
@@ -38,8 +38,8 @@ function init() {
   camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
   camera.position.set(0, 0, 80);
 
-  renderer = new THREE.WebGLRenderer({antialias:true});
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  const renderer = new THREE.WebGLRenderer({{ antialias: true }});
+  renderer.setSize(window.innerWidth * 0.95, window.innerHeight * 0.85);
   document.body.appendChild(renderer.domElement);
 
   controls = new THREE.OrbitControls(camera, renderer.domElement);
